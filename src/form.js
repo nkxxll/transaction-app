b1fce02646f8cf38e1338a12a2ace30344cdb32c
nkxxll <niki.nice1203@gmail.com>
2025-01-25 19:35:45 +0100

    base

---
+++ src/form.js
@@ -0,0 +1,5 @@
+import "./validators.js";
+
+export function createForm(element) {
+  element.innerHTML = `Hello from the form!`;
+}
---
+++ src/main.js
@@ -0,0 +1,19 @@
+import "./style.css";
+import "./form.js";
+import { createList } from "./list.js";
+import { createForm } from "./form.js";
+
+document.querySelector("#app").innerHTML = `
+  <div class="container">
+    <h1>Hello world!</h1>
+    <div id="form">
+    </div>
+    <div id="list">
+    </div>
+  </div>
+`;
+
+const form = document.getElementById("form");
+createForm(form);
+const list = document.getElementById("list");
+createList(list);
