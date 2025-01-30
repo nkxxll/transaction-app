4ab1da43f31b3387efec7b10971e2dcc90439e69
nkxxll <niki.nice1203@gmail.com>
2025-01-30 22:07:42 +0100

    list

--- src/main.js
+++ src/main.js
@@ -1,18 +1,42 @@
 import "./style.css";
 import "./form.js";
-import { createList } from "./list.js";
+import { createList, updateList } from "./list.js";
 import { addSubmitHandler, createForm } from "./form.js";
+
+// const transactions = [];
+
+// test data
+const transactions = [
+  {
+    amount: 100,
+    iban: "DE89370400440532013000",
+    recipientName: "John Doe",
+    executionDate: new Date(),
+    description: "Payment for services",
+  },
+  {
+    amount: 50,
+    iban: "FR1420041010050500013M02606",
+    recipientName: "Alice Smith",
+    executionDate: new Date(),
+    description: "Payment for goods",
+  },
+];
 
 document.querySelector("#app").innerHTML = `
     <h1>Hello world!</h1>
     <form id="form">
     </form>
-    <div id="list">
+    <div id="listAnchor">
     </div>
 `;
 
 const form = document.getElementById("form");
 createForm(form);
-addSubmitHandler(form);
-const list = document.getElementById("list");
-createList(list);
+
+const listAnchor = document.getElementById("listAnchor");
+const { list, placeholder } = createList(listAnchor);
+
+const update = updateList(list, placeholder, transactions);
+
+addSubmitHandler(form, update);
